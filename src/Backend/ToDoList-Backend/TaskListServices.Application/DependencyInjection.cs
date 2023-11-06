082b885d57eb3a726352d497e5ebfdc053643b48
salov-it <salovit96@gmail.com>
2023-11-06 14:05:56 +0300

    ref

---
+++ src/Backend/ToDoList-Backend/TaskListServices.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace UserServices.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddTaskListServices(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+         
+
+            return services;
+        }
+    }
+}
