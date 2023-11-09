51406d5bee0e560aa8971b3ed31b1333476ab294
salov-it <salovit96@gmail.com>
2023-11-09 14:09:55 +0300

    структура клиента

---
+++ src/Frontend/Frontend/TaskList-Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TaskList_Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
